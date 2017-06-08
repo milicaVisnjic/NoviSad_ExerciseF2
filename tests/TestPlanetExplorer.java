a3c691178f351364478a13bb7acbda6a3a671d43
somename <someemail>
2017-06-08 20:23:59 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,4 +8,25 @@ public class TestPlanetExplorer {
 	public void test_executeCommand() {
 		fail("Not yet implemented");
 	}
+	
+	@Test(expected=PlanetExplorerException.class)
+	public void testExceptionWithObstacleAt0_1() throws PlanetExplorerException {
+		PlanetExplorer p = new PlanetExplorer(3, 3, "(0,1)");
+		String kretanje = "ffrf";
+		String actualPosition=p.executeCommand(kretanje);
+		
+		//assertEquals("(1,1)", actualPosition);
+	}
+	
+	@Test
+	public void testCheckIfCarGoesRight() throws PlanetExplorerException {
+		PlanetExplorer p = new PlanetExplorer(3, 3, "(1,2)");
+		String kretanje = "f";
+		String actualPosition = p.executeCommand(kretanje);
+		
+		assertEquals("(0,1)", actualPosition);
+	}
+	
+	
+	
 }
